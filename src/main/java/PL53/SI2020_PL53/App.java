ae909185bc69fd5421ad6dbcea454f22a9b89ac1
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-02-19 11:57:55 +0100

    Created Maven project

---
+++ src/main/java/PL53/SI2020_PL53/App.java
@@ -0,0 +1,13 @@
+package PL53.SI2020_PL53;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/PL53/SI2020_PL53/AppTest.java
@@ -0,0 +1,20 @@
+package PL53.SI2020_PL53;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
