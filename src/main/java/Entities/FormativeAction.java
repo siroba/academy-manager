df086deedd0107d4abc1f729ea151bb62f6a4991
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-03-02 14:18:59 +0100

    Needed functions for the FormativeAction

---
+++ src/main/java/Entities/FormativeAction.java
@@ -0,0 +1,17 @@
+package Entities;
+
+import BaseProject.Database;
+
+public class FormativeAction {
+
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static FormativeAction obtain(int formativeActionID, Database database) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
