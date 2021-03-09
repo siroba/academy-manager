468d37c9818f6ce4d0df963ad7c596634ea70f66
Silvia Rodríguez Bares <silvibares@gmail.com>
2021-03-10 00:29:59 +0100

    Implemented user story 13580

--- src/main/java/PL53/SI2020_PL53/App.java
+++ src/main/java/PL53/SI2020_PL53/App.java
@@ -4,6 +4,7 @@ package PL53.SI2020_PL53;
 public class App {
 	public static void main(String[] args) {
 		UserStory13575.Controller c = new UserStory13575.Controller();
+		
 		c.initController();
 	}
 }
