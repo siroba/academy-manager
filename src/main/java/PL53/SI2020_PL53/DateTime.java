43346017b7a09bf8207d9b067683ad0e6975e5e0
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-03-09 10:43:55 +0100

    More fixes for reading and inserting into database (#11)
    * Duplicate things after merge (again)
    
    * General fixes to insertion and query from database

--- src/main/java/PL53/SI2020_PL53/DateTime.java
+++ src/main/java/PL53/SI2020_PL53/DateTime.java
@@ -7,12 +7,26 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Calendar;
 
+/**
+ * @author Marcos
+ * 
+ * Wrapper class for the {@link java.util.Date} and
+ * {@link java.sql.Timestamp} classes.<br/>
+ * The {@link java.util.Date} has most of its functionality deprecated
+ * (that's why I don't use the
+ * {@link java.util.Date#Date(int, int, int, int, int, int)} in this
+ * class' constructors), but they would be useful for us. <br/>
+ * Also, the {@link java.sql.Timestamp} class is mostly obsolete. Its
+ * only good use is to read/write from/to SQL databases. That's why the
+ * functions {@link #toTimestamp()} and {@link #parseString(String)}
+ * (Timestamps are stored as longs in the database) exist.
+ */
 public class DateTime extends Date {
 	// Auto-generated serial ID
 	private static final long serialVersionUID = 2169788639882609776L;
 
 	/**
-	 * {@link DateFormat} variable to format the dates 
+	 * {@link DateFormat} variable to format the dates
 	 */
 	public static final DateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
@@ -71,51 +85,29 @@ public class DateTime extends Date {
 		this.hour = 0;
 		this.minute = 0;
 	}
-	
+
 	/**
-	 * Returns a {@link java.sql.Timestamp} object. 
-	 * The {@link Date#toSQL()} returns a {@link java.sql.Date} object, which ignores the time 
+	 * Returns a {@link java.sql.Timestamp} object. The {@link Date#toSQL()} returns
+	 * a {@link java.sql.Date} object, which ignores the time
 	 * 
 	 * @return Timestamp
 	 */
 	public java.sql.Timestamp toTimestamp() {
 		return new java.sql.Timestamp(this.toMillis());
 	}
-	
+
 	/**
 	 * Parses a String containing the date and time to an object The input date must
 	 * be in format "yyyy-MM-dd HH:mm"
 	 *
 	 * @param datetime
 	 * @return
+	 * @throws ParseException
 	 */
-	public static DateTime parseString(String datetime) {
-		System.out.println(datetime);
-		String tmp[] = datetime.split(" ");
+	public static DateTime parseString(String datetime) throws ParseException {
+		java.util.Date d = dateformat.parse(datetime);
 
-		System.out.println(tmp);
-		String time[] = tmp[1].split(":");
-		String date[] = tmp[0].split("-");
-    
-		return new DateTime(Integer.parseInt(time[1]), Integer.parseInt(time[0]), Integer.parseInt(date[2]),
-				Integer.parseInt(date[1]), Integer.parseInt(date[0]));
-	}
-
-	/**
-	 * Parses a String containing the date and time to an object. It takes care of
-	 * the formatting.
-	 *
-	 * @param datetime
-	 * @return
-	 */
-	public static DateTime parseUnformattedString(String datetime) throws ParseException {
-		String tmp[] = dateformat.format(datetime).split(" ");
-
-		String time[] = tmp[0].split(":");
-		String date[] = tmp[1].split("-");
-
-		return new DateTime(Integer.parseInt(time[1]), Integer.parseInt(time[0]), Integer.parseInt(date[2]),
-				Integer.parseInt(date[1]), Integer.parseInt(date[0]));
+		return DateTime.fromMillis(d.getTime());
 	}
 
 	/**
@@ -130,13 +122,14 @@ public class DateTime extends Date {
 	}
 
 	/**
-	 * Uses the {@link #toLocalDateTime()} function combined with the {@link LocalDateTime#toEpochSecond(ZoneOffset)} (assumes UTC+1) * 1000L
+	 * Uses the {@link #toLocalDateTime()} function combined with the
+	 * {@link LocalDateTime#toEpochSecond(ZoneOffset)} (assumes UTC+1) * 1000L
 	 */
 	@Override
 	public long toMillis() {
-		return this.toLocalDateTime().toEpochSecond(ZoneOffset.ofHours(1))*1000l;
+		return this.toLocalDateTime().toEpochSecond(ZoneOffset.ofHours(1)) * 1000l;
 	}
-	
+
 	/**
 	 * Parses milliseconds to a {@link DateTime} object. <br/>
 	 * Uses the {@link Calendar#setTimeInMillis(long)} to then parse it with the
@@ -146,8 +139,8 @@ public class DateTime extends Date {
 	 * @return
 	 */
 	public static DateTime fromMillis(long millis) {
-		LocalDateTime ldt = LocalDateTime.ofEpochSecond(millis/1000L, 0, ZoneOffset.ofHours(1));
-		
+		LocalDateTime ldt = LocalDateTime.ofEpochSecond(millis / 1000L, 0, ZoneOffset.ofHours(1));
+
 		return new DateTime(ldt.getMinute(), ldt.getHour(), ldt.getDayOfMonth(), ldt.getMonthValue(), ldt.getYear());
 	}
 
@@ -170,7 +163,7 @@ public class DateTime extends Date {
 
 		return new DateTime(d.getMinute(), d.getHour(), d.getDayOfMonth(), d.getMonthValue(), d.getYear());
 	}
-	
+
 	/**
 	 * Same as {@link Date#daysSince(Date)}, but with minutes
 	 * 
@@ -180,7 +173,7 @@ public class DateTime extends Date {
 	public static int minutesSince(DateTime d) {
 		return minutesSince(d, DateTime.now());
 	}
-	
+
 	/**
 	 * Same as {@link Date#daysSince(Date, Date)}, but with minutes
 	 * 
@@ -196,12 +189,13 @@ public class DateTime extends Date {
 	}
 
 	/**
-	 * The returned string is formatted according to Spanish standards (dd/MM/yy HH:MM)
+	 * The returned string is formatted according to Spanish standards (dd/MM/yy
+	 * HH:MM)
 	 */
 	@Override
 	public String toString() {
-		String h = (this.hour<10?"0":"") + this.hour;
-		String m = (this.minute<10?"0":"") + this.minute;
+		String h = (this.hour < 10 ? "0" : "") + this.hour;
+		String m = (this.minute < 10 ? "0" : "") + this.minute;
 		return this.day + "/" + this.month + "/" + this.year + " " + h + ":" + m;
 	}
 
