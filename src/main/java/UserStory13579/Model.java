82c9d3d6483cf57dbe74aaba2ac6d0c00b1c48c4
vivien <vivien.drescher@rub.de>
2021-04-02 11:44:05 +0200

    Exception handeling

--- src/main/java/UserStory13579/Model.java
+++ src/main/java/UserStory13579/Model.java
@@ -1,10 +1,12 @@
 package UserStory13579;
 
 import java.sql.*;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 import Entities.Session;
+import PL53.util.Date;
 import PL53.util.DateTime;
 import Utils.Database;
 import Utils.UnexpectedException;
@@ -16,8 +18,9 @@ public class Model {
 	
 	/**
 	 * Gets the list of active races in object form for a given registration date.
+	 * @throws ParseException 
 	 */
-	public List<FinancialBalance> getListFinancialBalanceNoFilter() {
+	public List<FinancialBalance> getListFinancialBalance(Date startDate,Date endDate, String status){
 		//Query all rows from the result of a SQL query
 		try {
 		// Setup connection & statements 
@@ -29,30 +32,55 @@ public class Model {
 		
 		// Income confirmed
 		StringBuilder queryIncomeConfirmed = new StringBuilder();
-		queryIncomeConfirmed.append("select fa.nameFa, fa.status, ifnull(sum(CASE WHEN p.confirmed=true THEN p.amount END), 0) as income_confirmed  ");
-		queryIncomeConfirmed.append("from FormativeAction fA left join Enrollment e on fA.ID_fa=e.ID_fa left join invoice i on e.ID_professional=i.ID_professional AND e.ID_fa=i.ID_fa left join Payment p on p.ID_invoice=i.ID_invoice group by fa.ID_fa;");
+		queryIncomeConfirmed.append("select distinct(T.ID_fa), T.nameFa, T.status, T.income_confirmed from ");
+		queryIncomeConfirmed.append("(select fa.ID_fa, fa.nameFa, fa.status, ifnull(sum(CASE WHEN p.confirmed=true THEN p.amount END), 0) as income_confirmed  ");
+		queryIncomeConfirmed.append("from FormativeAction fA left join Enrollment e on fA.ID_fa=e.ID_fa left join invoice i on e.ID_professional=i.ID_professional AND e.ID_fa=i.ID_fa left join Payment p on p.ID_invoice=i.ID_invoice group by fa.ID_fa) T ");
+		queryIncomeConfirmed.append("left join Session s on s.ID_fa=T.ID_fa ");
+		queryIncomeConfirmed.append("where date(s.sessionStart) BETWEEN \"" + startDate.toSQL() + "\" AND \"" + endDate.toSQL() + "\" ");
+		if (status != null) queryIncomeConfirmed.append("and T.status=\"" + status.toUpperCase() + "\" ");
+		queryIncomeConfirmed.append(";");
 		ResultSet rsIncomeConfirmed=stmtIncomeConfirmed.executeQuery(queryIncomeConfirmed.toString());
 		
 		// Expenses confirmed
 		StringBuilder queryExpensesConfirmed = new StringBuilder();
-		queryExpensesConfirmed.append("select fa.nameFa, fa.status, ifnull(sum(CASE WHEN pt.confirmed THEN pt.amount END), 0) as expenses_confirmed ");
-		queryExpensesConfirmed.append("from FormativeAction fA left join InvoiceTeacher it on fA.ID_fa=it.ID_fa left join PaymentTeacher pt on pt.ID_invoice=it.ID_invoice group by fa.ID_fa;");
+		queryExpensesConfirmed.append("select distinct(T.ID_fa), T.nameFa, T.status, T.expenses_confirmed from ");
+		queryExpensesConfirmed.append("(select fa.ID_fa, fa.nameFa, fa.status, ifnull(sum(CASE WHEN pt.confirmed THEN pt.amount END), 0) as expenses_confirmed ");
+		queryExpensesConfirmed.append("from FormativeAction fA left join InvoiceTeacher it on fA.ID_fa=it.ID_fa left join PaymentTeacher pt on pt.ID_invoice=it.ID_invoice group by fa.ID_fa) T ");
+		queryExpensesConfirmed.append("left join Session s on s.ID_fa=T.ID_fa ");
+		queryExpensesConfirmed.append("where date(s.sessionStart) BETWEEN \"" + startDate.toSQL() + "\" AND \"" + endDate.toSQL() + "\" ");
+		if (status != null) queryExpensesConfirmed.append("and T.status=\"" + status.toUpperCase() + "\" ");
+		queryExpensesConfirmed.append(";");
 		ResultSet rsExpensesConfirmed=stmtExpensesConfirmed.executeQuery(queryExpensesConfirmed.toString());
 		
 		// Income estimated
 		StringBuilder queryIncomeEstimated = new StringBuilder();
-		queryIncomeEstimated.append("select  fa.nameFa, fa.status, ifnull(sum(CASE WHEN e.ID_fa THEN f.amount END), 0) as income_estimated ");
-		queryIncomeEstimated.append("from FormativeAction fA left join Fee f on f.id_fa=fa.ID_fa left join enrollment e on fa.ID_fa=e.ID_fa and e.category=f.category group by fa.ID_fa;");
+		queryIncomeEstimated.append("select distinct(T.ID_fa), T.nameFa, T.status, T.income_estimated from ");
+		queryIncomeEstimated.append("(select  fa.ID_fa, fa.nameFa, fa.status, ifnull(sum(CASE WHEN e.ID_fa THEN f.amount END), 0) as income_estimated ");
+		queryIncomeEstimated.append("from FormativeAction fA left join Fee f on f.id_fa=fa.ID_fa left join enrollment e on fa.ID_fa=e.ID_fa and e.category=f.category group by fa.ID_fa) T ");
+		queryIncomeEstimated.append("left join Session s on s.ID_fa=T.ID_fa ");
+		queryIncomeEstimated.append("where date(s.sessionStart) BETWEEN \"" + startDate.toSQL() + "\" AND \"" + endDate.toSQL() + "\" ");
+		if (status != null) queryIncomeEstimated.append("and T.status=\"" + status.toUpperCase() + "\" ");
+		queryIncomeEstimated.append(";");
 		ResultSet rsIncomeEstimated=stmtIncomeEstimated.executeQuery(queryIncomeEstimated.toString());
 			
 		// Expenses estimated
 		StringBuilder queryExpensesEstimated = new StringBuilder();
-		queryExpensesEstimated.append("select fa.nameFa, fa.status, sum(remuneration) as expenses_estimated  ");
-		queryExpensesEstimated.append("from FormativeAction fa left join Session s on fa.ID_fa=s.ID_fa group by fa.ID_fa;");
+		queryExpensesEstimated.append("select distinct(T.ID_fa), T.nameFa, T.status, T.expenses_estimated from ");
+		queryExpensesEstimated.append("(select fa.ID_fa, fa.nameFa, fa.status, s.sessionStart, sum(remuneration) as expenses_estimated  ");
+		queryExpensesEstimated.append("from FormativeAction fa left join Session s on fa.ID_fa=s.ID_fa group by fa.ID_fa) T ");
+		queryExpensesEstimated.append("where date(T.sessionStart) BETWEEN \"" + startDate.toSQL() + "\" AND \"" + endDate.toSQL() + "\" ");
+		if (status != null) queryExpensesEstimated.append("and T.status=\"" + status.toUpperCase() + "\" ");
+		queryExpensesEstimated.append(";");
 		ResultSet rsExpensesEstimated=stmtExpensesEstimated.executeQuery(queryExpensesEstimated.toString());
 		
 		// Sessions 
-		List<Session> sessions = Session.get("select * from session s", db);
+		StringBuilder querySessions = new StringBuilder();
+		querySessions.append("select * from session s ");
+		querySessions.append("left join FormativeAction fa on fa.ID_fa=s.ID_fa ");
+		querySessions.append("where date(s.sessionStart) BETWEEN \"" + startDate.toSQL() + "\" AND \"" + endDate.toSQL() + "\" ");
+		if (status != null) querySessions.append("and fa.status=\"" + status.toUpperCase() + "\" ");
+		querySessions.append(";");
+		List<Session> sessions = Session.get(querySessions.toString(), db);
 		List<DateTime> ListFaFirstSessionLastSession = getListFirstSessionLastSession(sessions);
 		
 		// Create list of Financial Balance objects 
@@ -90,11 +118,14 @@ public class Model {
 		return financialBalances;
 		} catch (SQLException e) { 
 			throw new UnexpectedException(e);
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
+		return new ArrayList<>();
 	}
 	
 	
-	public List<TotalBalance> getListTotalBalanceNoFilter() {
+	public List<TotalBalance> getListTotalBalance(Date startDate,Date endDate, String status) {
 		//Query all rows from the result of a SQL query
 		try {
 			
@@ -109,32 +140,51 @@ public class Model {
 		StringBuilder queryTotalIncomeConfirmed = new StringBuilder();
 		queryTotalIncomeConfirmed.append("select sum(income_confirmed) as total_income_confirmed ");
 		queryTotalIncomeConfirmed.append("FROM ");
-		queryTotalIncomeConfirmed.append("(select fa.nameFa, fa.status, ifnull(sum(CASE WHEN p.confirmed=true THEN p.amount END), 0) as income_confirmed ");
-		queryTotalIncomeConfirmed.append("from FormativeAction fA left join Enrollment e on fA.ID_fa=e.ID_fa left join invoice i on e.ID_professional=i.ID_professional AND e.ID_fa=i.ID_fa left join Payment p on p.ID_invoice=i.ID_invoice group by fa.ID_fa);");
+		queryTotalIncomeConfirmed.append("(select distinct(T.ID_fa), T.nameFa, T.status, T.income_confirmed from ");
+		queryTotalIncomeConfirmed.append("(select fa.ID_fa, fa.nameFa, fa.status, ifnull(sum(CASE WHEN p.confirmed=true THEN p.amount END), 0) as income_confirmed  ");
+		queryTotalIncomeConfirmed.append("from FormativeAction fA left join Enrollment e on fA.ID_fa=e.ID_fa left join invoice i on e.ID_professional=i.ID_professional AND e.ID_fa=i.ID_fa left join Payment p on p.ID_invoice=i.ID_invoice group by fa.ID_fa) T ");
+		queryTotalIncomeConfirmed.append("left join Session s on s.ID_fa=T.ID_fa ");
+		queryTotalIncomeConfirmed.append("where date(s.sessionStart) BETWEEN \"" + startDate.toSQL() + "\" AND \"" + endDate.toSQL() + "\" ");
+		if (status != null) queryTotalIncomeConfirmed.append("and T.status=\"" + status.toUpperCase() + "\" ");
+		queryTotalIncomeConfirmed.append(");");
 		ResultSet rsTotalIncomeConfirmed=stmtTotalIncomeConfirmed.executeQuery(queryTotalIncomeConfirmed.toString());
 		
 		// Total Expenses Confirmed 
 		StringBuilder queryTotalExpensesConfirmed = new StringBuilder();
 		queryTotalExpensesConfirmed.append("select sum(expenses_confirmed) as total_expenses_confirmed ");
 		queryTotalExpensesConfirmed.append("FROM ");
-		queryTotalExpensesConfirmed.append("(select fa.nameFa, fa.status, ifnull(sum(CASE WHEN pt.confirmed THEN pt.amount END), 0) as expenses_confirmed ");
-		queryTotalExpensesConfirmed.append("from FormativeAction fA left join InvoiceTeacher it on fA.ID_fa=it.ID_fa left join PaymentTeacher pt on pt.ID_invoice=it.ID_invoice group by fa.ID_fa);");
+		queryTotalExpensesConfirmed.append("(select distinct(T.ID_fa), T.nameFa, T.status, T.expenses_confirmed from ");
+		queryTotalExpensesConfirmed.append("(select fa.ID_fa, fa.nameFa, fa.status, ifnull(sum(CASE WHEN pt.confirmed THEN pt.amount END), 0) as expenses_confirmed ");
+		queryTotalExpensesConfirmed.append("from FormativeAction fA left join InvoiceTeacher it on fA.ID_fa=it.ID_fa left join PaymentTeacher pt on pt.ID_invoice=it.ID_invoice group by fa.ID_fa) T ");
+		queryTotalExpensesConfirmed.append("left join Session s on s.ID_fa=T.ID_fa ");
+		queryTotalExpensesConfirmed.append("where date(s.sessionStart) BETWEEN \"" + startDate.toSQL() + "\" AND \"" + endDate.toSQL() + "\" ");
+		if (status != null) queryTotalExpensesConfirmed.append("and T.status=\"" + status.toUpperCase() + "\" ");
+		queryTotalExpensesConfirmed.append(");");
 		ResultSet rsTotalExpensesConfirmed=stmtTotalExpensesConfirmed.executeQuery(queryTotalExpensesConfirmed.toString());
 		
 		// Total Income Estimated
 		StringBuilder queryTotalIncomeEstimated = new StringBuilder();
 		queryTotalIncomeEstimated.append("select sum(income_estimated) as total_income_estimated ");
 		queryTotalIncomeEstimated.append("FROM ");
-		queryTotalIncomeEstimated.append("(select  fa.nameFa, fa.status, ifnull(sum(CASE WHEN e.ID_fa THEN f.amount END), 0) as income_estimated ");
-		queryTotalIncomeEstimated.append("from FormativeAction fA left join Fee f on f.id_fa=fa.ID_fa left join enrollment e on fa.ID_fa=e.ID_fa and e.category=f.category group by fa.ID_fa);");
+		queryTotalIncomeEstimated.append("(select distinct(T.ID_fa), T.nameFa, T.status, T.income_estimated from ");
+		queryTotalIncomeEstimated.append("(select  fa.ID_fa, fa.nameFa, fa.status, ifnull(sum(CASE WHEN e.ID_fa THEN f.amount END), 0) as income_estimated ");
+		queryTotalIncomeEstimated.append("from FormativeAction fA left join Fee f on f.id_fa=fa.ID_fa left join enrollment e on fa.ID_fa=e.ID_fa and e.category=f.category group by fa.ID_fa) T ");
+		queryTotalIncomeEstimated.append("left join Session s on s.ID_fa=T.ID_fa ");
+		queryTotalIncomeEstimated.append("where date(s.sessionStart) BETWEEN \"" + startDate.toSQL() + "\" AND \"" + endDate.toSQL() + "\" ");
+		if (status != null) queryTotalIncomeEstimated.append("and T.status=\"" + status.toUpperCase() + "\" ");
+		queryTotalIncomeEstimated.append(");");
 		ResultSet rsTotalIncomeEstimated=stmtTotalIncomeEstimated.executeQuery(queryTotalIncomeEstimated.toString());
 		
 		// Total Expenses Estimated
 		StringBuilder queryTotalExpensesEstimated = new StringBuilder();
 		queryTotalExpensesEstimated.append("select sum(expenses_estimated) as total_expenses_estimated ");
 		queryTotalExpensesEstimated.append("FROM ");
-		queryTotalExpensesEstimated.append("(select fa.nameFa, fa.status, sum(remuneration) as expenses_estimated  ");
-		queryTotalExpensesEstimated.append("from FormativeAction fa left join Session s on fa.ID_fa=s.ID_fa group by fa.ID_fa);");
+		queryTotalExpensesEstimated.append("(select distinct(T.ID_fa), T.nameFa, T.status, T.expenses_estimated from ");
+		queryTotalExpensesEstimated.append("(select fa.ID_fa, fa.nameFa, fa.status, s.sessionStart, sum(remuneration) as expenses_estimated  ");
+		queryTotalExpensesEstimated.append("from FormativeAction fa left join Session s on fa.ID_fa=s.ID_fa group by fa.ID_fa) T ");
+		queryTotalExpensesEstimated.append("where date(T.sessionStart) BETWEEN \"" + startDate.toSQL() + "\" AND \"" + endDate.toSQL() + "\" ");
+		if (status != null) queryTotalExpensesEstimated.append("and T.status=\"" + status.toUpperCase() + "\" ");
+		queryTotalExpensesEstimated.append(");");
 		ResultSet rsTotalExpensesEstimated=stmtTotalExpensesEstimated.executeQuery(queryTotalExpensesEstimated.toString());
 		
 		// Create Total Balance object 
@@ -160,7 +210,10 @@ public class Model {
 		return totalBalance;
 		} catch (SQLException e) { 
 			throw new UnexpectedException(e);
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
+		return new ArrayList<>();
 	}
 	
 	/**
--- src/main/java/UserStory13579/View.java
+++ src/main/java/UserStory13579/View.java
@@ -12,12 +12,20 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+
+import PL53.swing.DateInput;
+import PL53.swing.DateTimeInput;
+import PL53.util.Date;
+import PL53.util.DateTime;
+
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JCheckBox;
@@ -30,18 +38,14 @@ import java.awt.event.ItemEvent;
 public class View {
 	
 	private JFrame frame;
-	private JCheckBox CBFilterStartDate;
-	public JComboBox CBStartDay;
-	public JComboBox CBStartMonth;
-	public JComboBox CBStartYear;
-	private JCheckBox CBFilterEndDate;
-	public JComboBox CBEndDay;
-	public JComboBox CBEndMonth;
-	public JComboBox CBEndYear;
 	public JScrollPane tableBalancePanel;
 	public JTable tableFormativeActions;
 	public JScrollPane tableTotalBalancePanel;
 	public JTable tableTotalBalance;
+	private DateInput start, end;
+	private JComboBox<String> status; 
+	private JButton btnApplyFilter;
+	private JCheckBox chckbxStartDate, chckbxEndDate, chckbxStatus; 
 
 	/**
 	 * Create the application.
@@ -54,91 +58,76 @@ public class View {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		Border blackline = BorderFactory.createLineBorder(Color.black);
+		
 		frame = new JFrame();
 		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 11));
-		frame.setTitle("FinancialBalance");
+		frame.setTitle("Financial Balance");
 		frame.setName("FinancialBalance");
 		frame.setBounds(0, 0, 1215, 400);
 		frame.setLocationRelativeTo(null);
 
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.getContentPane().setLayout(new MigLayout("", "[grow]", "[grow 10][grow][][]"));
-//		frame.getContentPane().setLayout(new MigLayout("", "[grow]", "[grow 10][][][][][grow][][]"));
+		frame.getContentPane().setLayout(new MigLayout("", "[grow]", "[grow][][][][][][][][]"));
 		
 		// Heading 
 		JLabel LabelHeading = new JLabel("Check Financial Balance ");
 		LabelHeading.setFont(new Font("Tahoma", Font.BOLD, 14));
-		frame.getContentPane().add(LabelHeading, "cell 0 0,aligny baseline");
+		frame.getContentPane().add(LabelHeading, "cell 0 0,alignx center,aligny center, height 50!");
 		
-//		// Date filter 
-//		JLabel LabelFilterDate = new JLabel("Filter by date:");
-//		LabelFilterDate.setFont(new Font("Tahoma", Font.BOLD, 11));
-//		frame.getContentPane().add(LabelFilterDate, "flowx,cell 0 1");
-//		
-//		// Start date 
-//		JCheckBox CBFilterStartDate = new JCheckBox("Start date:");
-//		CBFilterStartDate.setFont(new Font("Tahoma", Font.PLAIN, 11));
-//		frame.getContentPane().add(CBFilterStartDate, "cell 0 1,gapx 23");
-//		
-//		JComboBox CBStartDay = new JComboBox();
-//		CBStartDay.setFont(new Font("Times New Roman", Font.PLAIN, 10));
-//		CBStartDay.setModel(new DefaultComboBoxModel(new String[] {"Day", "01", "02 ", "03 ", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20 ", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
-//		frame.getContentPane().add(CBStartDay, "cell 0 1");
-//		
-//		CBStartMonth = new JComboBox();
-//		CBStartMonth.setFont(new Font("Tahoma", Font.PLAIN, 10));
-//		CBStartMonth.setModel(new DefaultComboBoxModel(new String[] {"Month", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"}));
-//		frame.getContentPane().add(CBStartMonth, "cell 0 1");
-//		
-//		JComboBox CBStartYear = new JComboBox();
-//		CBStartYear.setFont(new Font("Tahoma", Font.PLAIN, 10));
-//		CBStartYear.setModel(new DefaultComboBoxModel(new String[] {"Year", "2020", "2021", "2022", "2023", "2024", "2025"}));
-//		frame.getContentPane().add(CBStartYear, "cell 0 1");
-//		
-//		// End date 
-//		JCheckBox CBFilterEndDate = new JCheckBox("End date: ");
-//		CBFilterEndDate.setFont(new Font("Tahoma", Font.PLAIN, 11));
-//		frame.getContentPane().add(CBFilterEndDate, "flowx,cell 0 2,gapx 100");
-//		
-//		JComboBox CBEndDay = new JComboBox();
-//		CBEndDay.setFont(new Font("Tahoma", Font.PLAIN, 10));
-//		CBEndDay.setModel(new DefaultComboBoxModel(new String[] {"Day", "01", "02 ", "03 ", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20 ", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
-//		frame.getContentPane().add(CBEndDay, "cell 0 2");
-//		
-//		JComboBox CBEndMonth = new JComboBox();
-//		CBEndMonth.setFont(new Font("Tahoma", Font.PLAIN, 10));
-//		CBEndMonth.setModel(new DefaultComboBoxModel(new String[] {"Month", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"}));
-//		frame.getContentPane().add(CBEndMonth, "cell 0 2");
-//		
-//		JComboBox CBEndYear = new JComboBox();
-//		CBEndYear.setFont(new Font("Tahoma", Font.PLAIN, 10));
-//		CBEndYear.setModel(new DefaultComboBoxModel(new String[] {"Year", "2020", "2021", "2022", "2023", "2024", "2025"}));
-//		frame.getContentPane().add(CBEndYear, "cell 0 2");
-//		
-//		// Filter by status
-//		JLabel LabelFilterStatus = new JLabel("Filter by status:");
-//		LabelFilterStatus.setFont(new Font("Tahoma", Font.BOLD, 11));
-//		frame.getContentPane().add(LabelFilterStatus, "flowx,cell 0 3");
-//			
-//		ButtonGroup groupStatus = new ButtonGroup();
-//		
-//		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
-//		frame.getContentPane().add(rdbtnNewRadioButton, "cell 0 3");
-//		
-//		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("New radio button");
-//		frame.getContentPane().add(rdbtnNewRadioButton_1, "cell 0 3");
-//		
-//		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("New radio button");
-//		frame.getContentPane().add(rdbtnNewRadioButton_2, "cell 0 3");
-//		
-//		groupStatus.add(rdbtnNewRadioButton);
-//		groupStatus.add(rdbtnNewRadioButton_1);
-//		groupStatus.add(rdbtnNewRadioButton_2);
-//		
+		// Filter 
+		JLabel lblFilter = new JLabel("Filter:");
+		frame.getContentPane().add(lblFilter, "flowx,cell 0 1");
+			
+		// Filter by start date 
+		chckbxStartDate = new JCheckBox("Start date:");
+		chckbxStartDate.setSelected(true);
+		chckbxStartDate.setFont(new Font("Dialog", Font.PLAIN, 12));
+		frame.getContentPane().add(chckbxStartDate, "cell 0 1,gapx 50");
+		start = new DateInput();
+		int currentYear = Date.now().getYear();
+		frame.getContentPane().add(start, "cell 0 1,gapx 10,height 50!" );
+		start.getYearsTextField().setBound(2000, 3000);
+		start.getYearsTextField().setDefaultValue(currentYear);
+		start.getDaysTextField().setBound(1, 31);
+		start.getDaysTextField().setDefaultValue(1);
+		start.getMonthsTextField().setBound(1, 12);
+		start.getMonthsTextField().setDefaultValue(1);
+		start.setBorder(blackline);
+		
+		// Filter by end date
+		chckbxEndDate = new JCheckBox("End date:  ");
+		chckbxEndDate.setSelected(true);
+		chckbxEndDate.setFont(new Font("Dialog", Font.PLAIN, 12));
+		frame.getContentPane().add(chckbxEndDate, "cell 0 1,gapx 50");
+		end = new DateInput();
+		frame.getContentPane().add(end, "cell 0 1,gapx 10,height 50!" );
+		end.getYearsTextField().setBound(2000, 3000);
+		end.getYearsTextField().setDefaultValue(currentYear);
+		end.getDaysTextField().setBound(1, 31);
+		end.getDaysTextField().setDefaultValue(31);
+		end.getMonthsTextField().setBound(1, 12);
+		end.getMonthsTextField().setDefaultValue(12);
+		end.setBorder(blackline);
+		
+		// Filter by status
+		chckbxStatus = new JCheckBox("Status:");
+		chckbxStatus.setFont(new Font("Dialog", Font.PLAIN, 12));
+		frame.getContentPane().add(chckbxStatus, "cell 0 1,gapx 50,height 35!");
+		status = new JComboBox<String>();
+		status.setFont(new Font("Dialog", Font.PLAIN, 12));
+		status.setModel(new DefaultComboBoxModel<String>(new String[] {"Active", "Executed", "Cancelled"}));
+		frame.getContentPane().add(status, "cell 0 1,gapx 10");
+		
+		// Apply filter button 
+		btnApplyFilter = new JButton("Apply Filter");
+		frame.getContentPane().add(btnApplyFilter, "cell 0 1, gapx 70");
+		
 		// Financial balance 
 		JLabel LabelFinancialBalance = new JLabel("Financial balance");
 		LabelFinancialBalance.setFont(new Font("Tahoma", Font.BOLD, 12));
-		frame.getContentPane().add(LabelFinancialBalance, "cell 0 4");
+		frame.getContentPane().add(LabelFinancialBalance, "cell 0 5");
 		
 		tableFormativeActions = new JTable();
 		tableFormativeActions.setBackground(new Color(255, 255, 255));
@@ -146,12 +135,12 @@ public class View {
 		tableFormativeActions.setRowSelectionAllowed(false);
 		tableFormativeActions.setDefaultEditor(Object.class, null); //readonly
 		JScrollPane tablePanel = new JScrollPane(tableFormativeActions);
-		frame.getContentPane().add(tablePanel, "cell 0 5,grow");
+		frame.getContentPane().add(tablePanel, "cell 0 6,grow");
 		
 		// Total balance 
 		JLabel LabelTotalBalance = new JLabel("Total balance");
 		LabelTotalBalance.setFont(new Font("Tahoma", Font.BOLD, 12));
-		frame.getContentPane().add(LabelTotalBalance, "cell 0 6");
+		frame.getContentPane().add(LabelTotalBalance, "cell 0 7");
 		
 		tableTotalBalance = new JTable();
 		tableTotalBalance.setName("tableTotalBalance");
@@ -159,20 +148,20 @@ public class View {
 		tableTotalBalance.setDefaultEditor(Object.class, null); //readonly
 		tableTotalBalance.setBackground(Color.WHITE);
 		JScrollPane tableTotalBalancePanel = new JScrollPane(tableTotalBalance);
-		frame.getContentPane().add(tableTotalBalancePanel, "cell 0 7, grow");
+		frame.getContentPane().add(tableTotalBalancePanel, "cell 0 8, height 38!, width 654!");
+		
 		
 	}
 	
 	//Getters and Setters to access from the controller (compact representation)
 	public JFrame getFrame() { return this.frame; }
-//	public JCheckBox getFilterStartDay() {return this.CBFilterStartDate; }
-//	public JComboBox getStartDay() {return this.CBStartDay; }
-//	public JComboBox getStartMonth() {return this.CBStartMonth; }
-//	public JComboBox getStartYear() {return this.CBStartYear; }
-//	public JCheckBox getFilterEndDay() {return this.CBFilterEndDate; }
-//	public JComboBox getEndDay() {return this.CBEndDay; }
-//	public JComboBox getEndMonth() {return this.CBEndMonth; }
-//	public JComboBox getEndYear() {return this.CBEndYear; }
 	public JTable getTableFormativeActionBalance() { return this.tableFormativeActions; }
 	public JTable getTableTotalBalance() { return this.tableTotalBalance; }
+	public Date getStartDate() {return start.getDate();}
+	public Date getEndDate() {return end.getDate();}
+	public String getStatus() { return this.status.getSelectedItem().toString(); }
+	public boolean getFilterStartDate() {return this.chckbxStartDate.isSelected(); }
+	public boolean getFilterEndDate() {return this.chckbxEndDate.isSelected(); }
+	public boolean getFilterStatus() {return this.chckbxStatus.isSelected(); }
+	public JButton getBtnApplyFilter() { return this.btnApplyFilter; }
 }
