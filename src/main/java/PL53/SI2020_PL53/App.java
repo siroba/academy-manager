d095a3b2db4b168b7eba911018418065173d1415
vivien <vivien.drescher@rub.de>
2021-04-13 16:24:11 +0200

    Renamed functional packages

--- src/main/java/PL53/SI2020_PL53/App.java
+++ src/main/java/PL53/SI2020_PL53/App.java
@@ -3,7 +3,7 @@ package PL53.SI2020_PL53;
 
 public class App {
 	public static void main(String[] args) {
-		UserStory13575.Controller c = new UserStory13575.Controller();
+		RegisterPayment.Controller c = new RegisterPayment.Controller();
 		
 		c.initController();
 	}
