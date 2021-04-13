1f2c0fa8d4198f80c6f29cbd0f3a03585a3f9382
Marek <marek@ruschulte.com>
2021-04-13 16:48:41 +0200

    Merged dev in US13723

---
+++ src/main/java/PayATeacher/Data.java
@@ -0,0 +1,15 @@
+package PayATeacher;
+
+import Entities.Enrollment;
+import Entities.FormativeAction;
+import Entities.Invoice;
+import Entities.Payment;
+import Entities.PaymentTeacher;
+import Entities.Professional;
+
+public class Data {
+	public FormativeAction formativeAction;
+	public Invoice invoice;
+	public PaymentTeacher paymentTeacher;
+
+}
