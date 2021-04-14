c3e4fa6cd2a54b7b91614ba6c3f11135440ac77a
Marek <marek@ruschulte.com>
2021-04-14 19:51:09 +0200

    Adapt view to prototype

--- src/main/java/StatusOfFormativeAction/Model.java
+++ src/main/java/StatusOfFormativeAction/Model.java
@@ -1,77 +1,56 @@
 package StatusOfFormativeAction;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
+import Entities.FormativeAction;
 import PL53.util.Date;
 import Utils.Database;
 import Utils.UnexpectedException;
 
 public class Model {
-	
-	public List<String> getListFormativeAction() {
+
+	private Database db;
+
+	public Model() {
+		db = new Database();
+	}
+
+	public List<FormativeActionDetails> getListFormativeAction() {
 		try {
-			Connection cn=DriverManager.getConnection("jdbc:sqlite:database.db"); 
-			PreparedStatement ps = cn.prepareStatement("SELECT nameFa FROM FormativeAction;");
-			ResultSet rs=ps.executeQuery();
-			
-			List<String> formativeActionList = new ArrayList<String>();
-			
+			Connection cn = db.getConnection();
+			PreparedStatement ps = cn.prepareStatement("SELECT  fa.nameFa, fa.status, fa.enrollmentStart, fa.enrollmentEnd, fa.totalPlaces, (fa.totalPlaces - count(e.ID_fa)) as leftPlaces "
+					+ "FROM FormativeAction fa LEFT JOIN Enrollment e on e.ID_fa=fa.ID_fa "
+					+ "GROUP BY fa.ID_fa;");
+			ResultSet rs = ps.executeQuery();
+
+			List<FormativeActionDetails> formativeActionList = new ArrayList<FormativeActionDetails>();
+
 			while (rs.next()) {
-				formativeActionList.add(rs.getString("nameFa"));
+				formativeActionList.add(new FormativeActionDetails(rs.getString("nameFa"), rs.getString("status"),
+						Date.parse(rs.getTimestamp("enrollmentStart")), Date.parse(rs.getTimestamp("enrollmentEnd")),
+						rs.getInt("totalPlaces"), rs.getInt("leftPlaces")));
 			}
-			
+
 			rs.close();
 			ps.close();
 			cn.close();
-			
+
 			return formativeActionList;
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new UnexpectedException(e);
 		}
 	}
-	
-	public FormativeActionDetails getFormativeActionDetails(String selectedFormativeAction) {
-		try {
-			Connection cn=DriverManager.getConnection("jdbc:sqlite:database.db"); 
-			StringBuilder query = new StringBuilder();
-			query.append("SELECT  fa.nameFa, fa.status, fa.enrollmentStart, fa.enrollmentEnd, fa.totalPlaces, (fa.totalPlaces - count(e.ID_fa)) as leftPlaces ");
-			query.append("FROM FormativeAction fa LEFT JOIN Enrollment e on e.ID_fa=fa.ID_fa ");
-			query.append("WHERE fa.nameFa = ? ");
-			query.append("GROUP BY fa.ID_fa;");
-			PreparedStatement ps = cn.prepareStatement(query.toString());
-			ps.setString(1, selectedFormativeAction);
-			ResultSet rs=ps.executeQuery();			
-			
-			FormativeActionDetails fs = new FormativeActionDetails(
-					rs.getString("nameFa"),
-					rs.getString("status"), 
-					Date.parse(rs.getTimestamp("enrollmentStart")), 
-					Date.parse(rs.getTimestamp("enrollmentEnd")),
-					rs.getInt("totalPlaces"),
-					rs.getInt("leftPlaces"));
-			
-			rs.close();
-			ps.close();
-			cn.close();
-			return fs;
-		}
-		catch (SQLException e) {
-			throw new UnexpectedException(e);
-		}
-	}
-	
+
 	/*
 	 * TODO Description of Function, parameter and return value
-	 * */
+	 */
 	public List<Registration> getRegistrationList(String selectedFormativeAction) {
-		Database db = new Database();
 		try {
 			Connection cn = db.getConnection();
 			StringBuilder query = new StringBuilder();
@@ -84,27 +63,32 @@ public class Model {
 			query.append("INNER JOIN Fee ");
 			query.append("ON fa.ID_fa = Fee.ID_fa and Fee.category=e.category ");
 			query.append("WHERE lower(fa.nameFa) = lower(?);");
-						
+
 			PreparedStatement ps = cn.prepareStatement(query.toString());
 			ps.setString(1, selectedFormativeAction);
-			
+
 			ResultSet rs = ps.executeQuery();
-			
+
 			List<Registration> registrationLists = new ArrayList<>();
-						
+
 			while (rs.next()) {
-				Registration list = new Registration(
-						rs.getString("name"),
-						rs.getString("surname"),
-						Date.parse(rs.getTimestamp("timeEn")),
-						rs.getFloat("amount"),
-						rs.getString("status"));
+				Registration list = new Registration(rs.getString("name"), rs.getString("surname"),
+						Date.parse(rs.getTimestamp("timeEn")), rs.getFloat("amount"), rs.getString("status"));
 				registrationLists.add(list);
 			}
-			
+
 			return registrationLists;
 
 		} catch (SQLException e) {
+			throw new UnexpectedException(e);
+		}
+	}
+	
+	public FormativeAction getFormativeAction(String name) {
+		String query = "SELECT * from FormativeAction fa WHERE fa.nameFa = '" + name + "';";
+		try {
+			return FormativeAction.getOne(query, db);
+		} catch (SQLException | ParseException e) {
 			throw new UnexpectedException(e);
 		}
 	}
