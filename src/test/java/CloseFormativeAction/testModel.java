f80fbbeb31e32f47941d419efaeedce58a610e4f
Marek <marek@ruschulte.com>
2021-05-05 22:22:11 +0200

    Worked on tests

--- src/test/java/CloseFormativeAction/testModel.java
+++ src/test/java/CloseFormativeAction/testModel.java
@@ -2,6 +2,7 @@ package CloseFormativeAction;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import Utils.Database;
 
@@ -19,13 +20,11 @@ public class testModel {
 	}
 	
 	public static void loadCleanDatabase(Database db) {
-		db.executeBatch(new String[] {
-				"delete from FormativeAction",
-				"insert into FormativeAction (ID_fa, nameFa, totalPlaces, objectives, mainContent, status, enrollmentStart, enrollmentEnd) values\n"
-				+ "	(1000, 'Initiation to scrum',	6,	'some objctives', 'some main content', 'EXECUTED', '2021-01-01 00:00:00.0',	'2021-02-01 00:00:00.0'),"
-				+ "	(1001, 'Initiation to agile',	10, 'some objctives', 'some main content', 'EXECUTED', '2021-01-01 00:00:00.0',	'2021-02-08 00:00:00.0'),"
-				+ "	(1002, 'JDBC',  		 		4,  'some objctives', 'some main content', 'ACTIVE',   '2021-03-01 00:00:00.0',	'2021-04-08 00:00:00.0'),"
-				+ "	(1003, 'Java programming',	 	5,	'some objctives', 'some main content', 'ACTIVE',   '2021-04-01 00:00:00.0',	'2021-04-28 00:00:00.0');"
-		});
+		db.loadDatabase();
+	}
+	
+	@Test
+	public void testLALA() {
+		
 	}
 }
