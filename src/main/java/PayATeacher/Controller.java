0d76b750fb3885a1e8170eb951b6ecd7dd535e31
Silvia Rodríguez Bares <silvibares@gmail.com>
2021-05-06 11:45:36 +0200

    Fixed commit conflict

--- src/main/java/PayATeacher/Controller.java
+++ src/main/java/PayATeacher/Controller.java
@@ -7,11 +7,13 @@ import java.awt.event.MouseEvent;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
+import BaseProject.SwingUtil;
 import Entities.FormativeAction;
 import Entities.MovementTeacher;
 import Entities.PaymentTeacher;
@@ -23,6 +25,7 @@ import PL53.util.DateTime;
 import PayATeacher.Data;
 import PayATeacher.Model;
 import PayATeacher.View;
+import RegisterPayment.AuxPayment;
 
 public class Controller implements PL53.util.Controller {
 	private Model model;
@@ -32,29 +35,16 @@ public class Controller implements PL53.util.Controller {
 
 	public Controller() {
 		this.model = new Model();
+
 		try {
 			model.initModel();
-		} catch (SQLException e) {
-
-			e.printStackTrace();
-		} catch (ParseException e) {
-
+		} catch (SQLException | ParseException e) {
 			e.printStackTrace();
 		}
-		this.view = new View();
 
+		this.view = new View();
 		this.initView();
 	}
-
-	/**
-	 * Controller initialization: add event handlers to the UI objects. Each event
-	 * handler is instantiated in the same way, so that it invokes a private method
-	 * of this handler, enclosed in a generic exception handler to display windows.
-	 * Each event handler is instantiated in the same way, so that it invokes a
-	 * private method of this controller, enclosed in a generic exception handler to
-	 * display popup windows when a problem or exception occurs. popup windows when
-	 * a problem or controlled exception occurs.
-	 */
 
 	public void initController() {
 		view.getTable().addMouseListener(new MouseAdapter() {
@@ -64,6 +54,9 @@ public class Controller implements PL53.util.Controller {
 
 				if (selectedRow == null) {
 					JOptionPane.showMessageDialog(null, "You have to select one payment");
+				} else if (selectedRow.movementTeacher != null) {
+					TableModel tm = showPayments(model.getAllData(selectedRow));
+					view.setMovementTable(tm);
 				}
 
 			}
@@ -72,11 +65,41 @@ public class Controller implements PL53.util.Controller {
 		view.getRegisterButton().addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
+				DateTime now = DateTime.now();
+				
+				// Data from the form
+				String name = view.getNameTextField();
+				String surname = view.getSurname().getText();
+				String fiscalNumber = view.getFiscalNumberTextField();
+				String address = view.getAddressTextField();
+				Date dateTransfer = view.getDateTransferTextField();
+				Date dateInvoice = view.getDateTextField();
+				String IDInvoice = view.getIDInvoice();
 
+				int ID_teacher = selectedRow.teacher.getID();
+				int ID_fa = selectedRow.formativeAction.getID();
+				
+				// Data from the database
+				//float professionalPaid = model.getAmountPaid(selectedRow); // Sum of all the payments to COIIPA
+				//float refundedAmount = model.getAmountReturned(selectedRow);// Sum of all the payments to the professional
+				//float totalPaid = model.getAmountTotalPaid(selectedRow);	// Sum of all the payments
+				String fiscalNumberDB = model.getFiscalNumber(selectedRow); //new Teacher((String) view.getTable().getValueAt(view.getSelected(), 2), (String) view.getTable().getValueAt(view.getSelected(), 3), "", "", ""));
+
+				float remuneration = selectedRow.teacherTeaches.getRemuneration();
+				String sender = "COIIPA";
+
+				// Payment calculations
+				//float fee = selectedRow.invoice.getAmount();
+				//float newTotal = totalPaid + newPayment;
+				//float toReturn = newTotal - fee;
+
+				// Dates...
+				int daysBetweenNowAction = DateTime.daysSince(dateTransfer, now);
+			
+				/////////////////////////////////////////////////////////////////////////////////////
+				
 				// INPUT THE INVOICE
 				try {
-					DateTime now = DateTime.now();
-					long daysBetweenNowAction = DateTime.daysSince(view.getDateTransferTextField(), now);
 					if (selectedRow == null) {
 						JOptionPane.showMessageDialog(null, "You have to select one pending payment");
 					} else if (view.getNameTextField().length() == 0) {
@@ -85,42 +108,26 @@ public class Controller implements PL53.util.Controller {
 						JOptionPane.showMessageDialog(null, "You have to introduce the fiscal number");
 					} else if (!Teacher.checkFiscalNumber(view.getFiscalNumberTextField())) {
 						JOptionPane.showMessageDialog(null,
-							    "Please provide a valid fiscal number. E.g. \\\"55566677R\\.",
+							    "Please provide a valid fiscal number. E.g. \"55566677R\".",
 							    "Invalid fiscal number",
 							    JOptionPane.ERROR_MESSAGE);
 					} else if (view.getAddressTextField().length() == 0) {
 						JOptionPane.showMessageDialog(null, "You have to introduce the address");
 					} else if (view.getDateTransferTextField().getYear() == 0) {
-						JOptionPane.showMessageDialog(null,
-								"You have to introduce a valid year for the date of the transfer (ex: 2021) ");
+						JOptionPane.showMessageDialog(null, "You have to introduce a valid year for the date of the transfer (ex: 2021) ");
 					} else if (view.getDateTextField().getYear() == 0) {
-						JOptionPane.showMessageDialog(null,
-								"You have to introduce a valid year for the date of the invoice (ex: 2021)");
+						JOptionPane.showMessageDialog(null, "You have to introduce a valid year for the date of the invoice (ex: 2021)");
 					} else if (view.getIDInvoice().length() == 0) {
 						JOptionPane.showMessageDialog(null, "You have to introduce the ID of the invoice");
 					} else if (view.getAmount() < 0) {
 						JOptionPane.showMessageDialog(null, "You have to introduce the amount of the invoice");
-
 					} else if (daysBetweenNowAction > 0) {
-
-						JOptionPane.showMessageDialog(null, "Tranfers cannot be made in the future.",
-								"date not valid ( the date must be on the current date or in the past)",
+						JOptionPane.showMessageDialog(null, 
+								"Tranfers cannot be made in the future.",
+								"Date not valid ( the date must be on the current date or in the past)",
 								JOptionPane.ERROR_MESSAGE);
-
 					} else {
-						String name = view.getNameTextField();
-						String surname = view.getSurname().getText();
-						String fiscalNumber = view.getFiscalNumberTextField();
-						
-						String address = view.getAddressTextField();
-						Date dateTransfer = view.getDateTransferTextField();
-						int ID_fa = selectedRow.formativeAction.getID();
-						float amountAgreed = Float
-								.parseFloat((String) view.getTable().getValueAt(view.getSelected(), 4));
-						String sender = "COIIPA";
-						String receiver = (String) view.getTable().getValueAt(view.getSelected(), 2) + " " + (String) view.getTable().getValueAt(view.getSelected(), 3) ;
-						String fiscalNumberDB = model.getFiscalNumber(new Teacher((String) view.getTable().getValueAt(view.getSelected(), 2), (String) view.getTable().getValueAt(view.getSelected(), 3), "", "", ""));
-						boolean confirmed = true;
+						//Amount agreed 
 						// CHeck if fiscal number stored in the DB matches the entered one & provide the option to update it in the db if not 
 						if (fiscalNumber != fiscalNumberDB) {
 							int dialogButton = JOptionPane.YES_NO_CANCEL_OPTION;
@@ -136,26 +143,47 @@ public class Controller implements PL53.util.Controller {
 				            	return;
 				            }	
 						}
-						Date dateInvoice = view.getDateTextField();
-						String IDInvoice = view.getIDInvoice();
-						int ID_teacher = selectedRow.teacher.getID();
-						float amount = view.getAmount();
-
+						
 						// TODO: The amount of the invoice and the payment can differ
 
-						MovementTeacher invoice = new MovementTeacher(IDInvoice, amount, ID_fa, dateInvoice, sender,
-								receiver, fiscalNumber, address, ID_teacher, ""); // TODO: Description
+						if (amount > remuneration) {
+							String str = String.format("%.2f", (remuneration - amount));
+						
+							int option = JOptionPane.showConfirmDialog(null, "The amount inputted (" + amount
+									+ ") is hihger than the amount agreed, Do you want to return the diferrence ("
+									+ str + ")?", "warning",
+											JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE));
 
-						PaymentTeacher paymentTeacher = new PaymentTeacher(IDInvoice, amount, dateTransfer, confirmed, ""); // TODO: Description
+							if (option == 0) {
+								float toReturn = amountAgreed - amount;
+
+								try {
+									model.createPaymentRefund(selectedRow.movementTeacher.getID(), -toReturn,
+											dateInvoice, false, true);
+								} catch (SQLException | ParseException e1) {
+									e1.printStackTrace();
+								}
+
+								JOptionPane.showMessageDialog(null,
+									"The invoice has been successfully created and the payment has been attached");
+							}
+						}
+						if (amount < remuneration) {
+							JOptionPane.showMessageDialog(null,
+									"The amount inputted is lower than  the agreed amount ");
+
+						}
+
+						MovementTeacher invoice = new MovementTeacher(IDInvoice, amount, ID_fa, dateInvoice, sender,
+								receiver, fiscalNumber, address, ID_teacher, "");
+
+						PaymentTeacher paymentTeacher = new PaymentTeacher(IDInvoice, amount, dateTransfer, true, "");
 
 						model.insertInvoice(invoice, paymentTeacher);
 						if (paymentTeacher != null) {
-
-							JOptionPane.showMessageDialog(null,
-									"The invoice has been successfully created and the payment has been attached");
+							JOptionPane.showMessageDialog(null, "The invoice has been successfully created and the payment has been attached");
 
 							model.initModel();
-
 							initView();
 						}
 					}
@@ -165,13 +193,39 @@ public class Controller implements PL53.util.Controller {
 					JOptionPane.showMessageDialog(null, "Error creating the invoice");
 					e1.printStackTrace();
 				} catch (ParseException e1) {
-
 					e1.printStackTrace();
 				}
 
 			}
 		});
-	}
+
+		// Function to add movements
+		view.getAddMovement().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				boolean teacher = view.getCheckTeacher();
+				boolean COIIPA = view.getCheckCOIIPA();
+				
+				float amount = view.getAmoundRefound();
+				Date payDate = view.getDateTransferTextField_1();
+				// Movement made by the teacher to refund money
+				if (teacher == true) {
+					
+
+				}
+
+				// Movement made by COIIPA to the teacher
+				if (COIIPA == true) {
+
+				}
+				
+				else {
+					JOptionPane.showMessageDialog(null, "You must select the sender of the movement",
+							"Select sender",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
 
 	public void initView() {
 
@@ -192,7 +246,8 @@ public class Controller implements PL53.util.Controller {
 			for (TeacherTeaches tt : d.formativeAction.getTeacherTeaches()) {
 
 				rows.add(new String[] { d.formativeAction.getName(), d.formativeAction.getStatus().toString(),
-						tt.getTeacher().getName(), tt.getTeacher().getSurname(), Float.toString(tt.getRemuneration()) });
+						tt.getTeacher().getName(), tt.getTeacher().getSurname(),
+						Float.toString(tt.getRemuneration()) });
 			}
 
 		}
@@ -213,6 +268,29 @@ public class Controller implements PL53.util.Controller {
 		}
 
 		return tm;
+	}
+
+	public TableModel showPayments(Data[] pt) {
+		String header[] = { "Sender", "Receiver", "date", "Amount" };
+
+		String body[][] = new String[pt.length][header.length];
+
+		for (int i = 0; i < pt.length; i++) {
+			Data d = pt[i];
+			body[i] = new String[] { d.movementTeacher.getSender(), d.movementTeacher.getReceiver(),
+					d.paymentTeacher.getPayDate().toString(), Float.toString(d.paymentTeacher.getAmount()) };
+		}
+
+		TableModel tm = new DefaultTableModel(header, body.length);
+
+		for (int i = 0; i < body.length; i++) {
+			for (int j = 0; j < header.length; j++) {
+				tm.setValueAt(body[i][j], i, j);
+			}
+		}
+
+		return tm;
+
 	}
 
 }
