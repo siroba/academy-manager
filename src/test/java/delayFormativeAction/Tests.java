9ce3873fe1aa4c780ea2037007f050725251f8af
Marcos Gutiérrez Alonso <margual56@gmail.com>
2021-05-11 18:24:57 +0200

    Create Tests.java

---
+++ src/test/java/delayFormativeAction/Tests.java
@@ -0,0 +1,25 @@
+package delayFormativeAction;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Tests {
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void randomTest() {
+		assertEquals(1,1);
+	}
+	
+}
